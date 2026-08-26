151e2c9c79d9470a9a6c089bd6595d1785abe9e1
RAdKuzur <drive16052003@gmail.com>
2026-08-26 10:06:57 +0400

    init

---
+++ server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	http.ListenAndServe(":6611", r)
+}
